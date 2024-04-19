7f60dd573a1a09b3be98d5ba6b55f7a1b416da85
Amirhosein Rostami <amirhosein.rostami.work@gmail.com>
2024-04-20 01:11:06 +0330

    Add header authentication to okhttp client

--- app/src/main/java/com/nocompany/catslist/di/AppModule.kt
+++ app/src/main/java/com/nocompany/catslist/di/AppModule.kt
@@ -5,6 +5,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
@@ -13,10 +14,31 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+    private const val API_KEY ="WELDY"
+
     @Provides
     @Singleton
-    fun provideCatsApi(): CatsApi = Retrofit
+    fun provideClientWithCache(): OkHttpClient {
+        return OkHttpClient
+            .Builder()
+            .addInterceptor {
+                it.run {
+                    proceed(
+                        request()
+                            .newBuilder()
+                            .addHeader("x-api-key", API_KEY)
+                            .build()
+                    )
+                }
+            }
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideCatsApi(client: OkHttpClient): CatsApi = Retrofit
         .Builder()
+        .client(client)
         .baseUrl(CatsApi.BASE_URL)
         .addConverterFactory(MoshiConverterFactory.create())
         .build()
