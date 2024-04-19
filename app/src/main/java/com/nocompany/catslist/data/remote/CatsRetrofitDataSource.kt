9cc76337b6163a0f534a8c152ef1400dd59ea6bf
Amirhosein Rostami <amirhosein.rostami.work@gmail.com>
2024-04-19 17:54:28 +0330

    Make get list suspend function

--- app/src/main/java/com/nocompany/catslist/data/remote/CatsRetrofitDataSource.kt
+++ app/src/main/java/com/nocompany/catslist/data/remote/CatsRetrofitDataSource.kt
@@ -4,7 +4,7 @@ import com.nocompany.catslist.data.remote.dto.CatDto
 import javax.inject.Inject
 
 class CatsRetrofitDataSource @Inject constructor(private val api: CatsApi) : CatsRemoteDataSource {
-    override fun getCatsList(page: Int, limit: Int): List<CatDto> {
+    override suspend fun getCatsList(page: Int, limit: Int): List<CatDto> {
         return api.getCatsList(page, limit)
     }
 }
