42615a8e23e1a94787ea9ae29ef150cf7ef708b3
Amirhosein Rostami <amirhosein.rostami.work@gmail.com>
2024-04-20 20:22:27 +0330

    Remove not needed BookmarkableCat model

--- app/src/main/java/com/nocompany/catslist/presentation/mapper/CatMapper.kt
+++ app/src/main/java/com/nocompany/catslist/presentation/mapper/CatMapper.kt
@@ -1,11 +1,8 @@
 package com.nocompany.catslist.presentation.mapper
 
-import com.nocompany.catslist.domain.model.BookmarkableCat
 import com.nocompany.catslist.domain.model.Cat
 import com.nocompany.catslist.presentation.model.CatModel
 
-fun BookmarkableCat.asPresentationCatModel(): CatModel =
-    CatModel(cat.id, cat.url, cat.width, cat.height, isBookmarked)
 
 fun CatModel.asDomainCatModel(): Cat =
     Cat(id, url, width, height)
