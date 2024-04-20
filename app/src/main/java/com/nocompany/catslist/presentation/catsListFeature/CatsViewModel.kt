351ba21d3326d8abbe75340c2cc6e9a4b4a50c12
Amirhosein Rostami <amirhosein.rostami.work@gmail.com>
2024-04-20 14:28:32 +0330

    Implement bookmark method in viewmodel

--- app/src/main/java/com/nocompany/catslist/presentation/catsListFeature/CatsViewModel.kt
+++ app/src/main/java/com/nocompany/catslist/presentation/catsListFeature/CatsViewModel.kt
@@ -4,17 +4,31 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
 import androidx.paging.map
+import com.nocompany.catslist.domain.BookmarkCatsUseCase
 import com.nocompany.catslist.domain.GetCatsUseCase
+import com.nocompany.catslist.presentation.mapper.asDomainCatModel
 import com.nocompany.catslist.presentation.mapper.asPresentationCatModel
+import com.nocompany.catslist.presentation.model.CatModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class CatsViewModel @Inject constructor(private val getCatsUseCase: GetCatsUseCase) : ViewModel() {
+class CatsViewModel @Inject constructor(
+    private val getCatsUseCase: GetCatsUseCase,
+    private val bookmarkUseCase: BookmarkCatsUseCase,
+) : ViewModel() {
     val cats = getCatsUseCase()
         .map { pagingData ->
             pagingData.map { it.asPresentationCatModel() }
         }
         .cachedIn(viewModelScope)
+
+
+    fun bookmark(catModel: CatModel, bookMark: Boolean) {
+        viewModelScope.launch {
+            bookmarkUseCase(catModel.asDomainCatModel(), bookMark)
+        }
+    }
 }
